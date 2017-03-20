b04436e1aaf4f1cf94e782373d3d187e5a8ba525
mchdd <mchdd@localhost.localdomain>
2017-03-20 04:06:53 +0300

    intro mchd for learning for loop

--- mchd-for-alghoritm.c
+++ mchd-for-alghoritm.c
@@ -2,105 +2,105 @@
 int main (){
       int space;
       int dx, dy, dz, dt, da;
-
+      printf ("Enter a character: ");
+      char ch = getchar();
+      printf ("Character is %c\n\n\n", ch);
       //for (dx=0; dx<=7; dx++){
       //      printf (" ");
-      //      for (dy=0; dy<=7-dx; dy++){
-      //            printf (" ");
-      //      }
-      //      printf ("\n");
-      //}
-      printf ("            *\n          * * *\n        * *   * *\n"
-      "      * *       * *\n    * *           * *\n  * *               * *\n"
-      "* *                   * *\n");
+      //      for (dy=0; dy<=7-dx; dy++){ //printf (" "); } //printf ("\n") }
+
+      printf ("            %c\n          %c %c %c\n        %c %c   %c %c\n"
+      "      %c %c       %c %c\n    %c %c           %c %c\n  %c %c               %c %c\n"
+      "%c %c                   %c %c\n",ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch
+,ch,ch,ch,ch,ch,ch,ch,ch,ch);
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n\n\n");
 
 
       int hx, hy;
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (hx=1; hx<=7; hx++){
             for (hy=1; hy<=11; hy++){
                   printf (" ");
             }
-            printf ("* *");
+            printf ("%c %c",ch,ch);
             printf ("\n");
       }
 
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
             printf ("\n");
       for (space=1; space<=13; space++){
-                  printf ("* ");
+                  printf ("%c ",ch);
             }
             printf ("\n\n\n");
 
 
       int cx, cy;
       for (cx=1; cx<=7; cx++){
-            printf ("* *");
+            printf ("%c %c",ch,ch);
             for (cy=1; cy<=19; cy++){
                   printf (" ");
             }
-            printf ("* *");
+            printf ("%c %c",ch,ch);
             printf (" ");
             printf ("\n");
       }
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n\n\n");
 
 
       int  mj, ml;
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (ml=6; ml>=1; ml--){
             for (mj=1; mj<=7-ml; mj++){
                   printf ("  ");
             }
-            printf ("* *");
+            printf ("%c %c",ch,ch);
             printf ("\n");
       }
-      printf ("             * *\n");
+      printf ("             %c %c\n",ch,ch);
       int mx, my;
       for (mx=1; mx<=6; mx++){
             for (my=6-mx; my>=0; my--){
                   printf ("  ");
             }
-            printf ("* *");
+            printf ("%c %c",ch,ch);
             printf ("\n");
       }
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
       printf ("\n");
       for (space=1; space<=13; space++){
-            printf ("* ");
+            printf ("%c ",ch);
       }
 printf ("\n");
 return 0;
