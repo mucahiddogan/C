267c3ea8953bfb5ad60f93da563c14666e2c32ac
mchdd <mchdd@localhost.localdomain>
2017-03-23 09:22:52 +0300

    finding the hipotenus

--- hipotenus.c
+++ hipotenus.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-float hipotenus(float c, float a, float b);
+float hipotenus(float a, float b);
 
-float hipotenus (float c, float a, float b){
-      c =  pow(a*a + b*b, 1/2);
-}
+
 
 int main (){
       float a, b, c;
       printf ("Enter a and b: ");
       scanf ("%f%f", &a, &b);
-      //c = hipotenus(c, a, b);
-      c = pow(a*a + b*b, 1.0/2);
+      c = hipotenus(a, b);
+      //c = pow(a*a + b*b, 1.0/2);
       printf ("C = %f", c);
       return 0;
+}
+
+float hipotenus (float a, float b){
+      float c;
+      c =  pow(a*a + b*b, 1.0/2);
+      //pow(a*a + b*b, 1.0/2);
+      //printf ("c= %f\n", c);
+      return c;
 }
