17c92a89840cba591ef0b2892a3e88d31f1025df
mchdd <mchdd@localhost.localdomain>
2017-04-16 20:35:46 +0300

    dizi ile girilen karakterin sayısını hesaplama

---
+++ karakter_sayisi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	char metin[30];
+	int i, counter=0;
+	printf("metni girin: ");
+	scanf( "%s", metin);
+
+	printf("girdiginiz metin: ");
+	for( i = 0; metin[i]!='\0'; i++){
+		printf("%c", metin[i]);
+            counter++;
+      }
+	printf("\n%d\n", counter);
+
+	return 0;
+}
